ef3a8edbf4c97d7e05238882a7b17aedcf6a7cc0
ekene9999 <ekeneiwenjora@gmail.com>
2022-06-26 21:21:08 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Quill from './quill';
+import Editor from "./quill"
+import React from 'react';
+function App() {
+  return (
+    <div>
+      <Editor />
+    </div>
+  );
+}
+
+export default App;
